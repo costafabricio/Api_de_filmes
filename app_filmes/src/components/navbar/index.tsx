e2fe92eae8091fc8324d6204ecc04c6322289f58
fabricio <fabriciosantosc11@gmail.com>
2026-04-20 10:40:27 -0300

    criando barra de pesquisa de filme

--- app_filmes/src/components/navbar/index.tsx
+++ app_filmes/src/components/navbar/index.tsx
@@ -1,9 +1,40 @@
+"use client"
+
+import { useState } from 'react'
+
 import './index.scss'
+import { BsSearch } from 'react-icons/bs'
 
 export default function Navbar() {
+    const [input, setInput] = useState("")
+
+    function onResearch(e: FormEvent) {
+        e.preventDefault()
+
+        if(input === '') return
+
+       navigate(`/detail/${input}`)
+    }
+
     return(
         <nav className="navbar">
             <h1 className="page-title">Filmes</h1>
+
+            <div>
+             <form onSubmit={onResearch} className='form'>
+              <input 
+              type="text" 
+              placeholder='Pesquisar' 
+              className='research'
+              value={input}
+              onChange={(e) => setInput(e.target.value)} 
+              />
+               <button type='submit' className='button'>
+                  <BsSearch size={30} color="#fff"/>
+               </button>
+             </form>
+            </div>
+
         </nav>
     )
 }
